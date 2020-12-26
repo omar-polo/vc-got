406d7b4d0d3443ace9ec9f938296041977d918eb
Omar Polo <op@omarpolo.com>
2020-12-26 10:09:45 +0000

    switch to GPLv3
    and while here adjust slightly the comment headers.

--- vc-got-stage.el
+++ vc-got-stage.el
@@ -1,4 +1,17 @@
-;; vc-got-stage.el --- Stage changes in vc-got diff buffers  -*- lexical-binding: t; -*-
+;;; vc-got-stage.el --- Stage changes diff buffers   -*- lexical-binding: t; -*-
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 (eval-when-compile
   (require 'subr-x))
